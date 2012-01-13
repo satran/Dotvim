026af91bcf95143ff9f7f4132f11ac2228d8f250
Satyajit Ranjeev <satyajit.ranjeev@gmail.com>
2012-01-13 15:13:36 +0530

    Added new plugin - vim power status.

--- vim-git-script.sh
+++ vim-git-script.sh
@@ -13,6 +13,7 @@ git submodule add https://github.com/tomtom/tcomment_vim.git bundle/tcomment_vim
 git submodule add https://github.com/vim-scripts/taglist.vim.git bundle/taglist.vim
 git submodule add https://github.com/fholgado/minibufexpl.vim.git bundle/minibufexpl.vim
 git submodule add https://github.com/klen/python-mode.git bundle/python-mode
+git submodule add https://github.com/Lokaltog/vim-powerline.git bundle/vim-powerline
 git submodule init
 git submodule update
 git submodule foreach git submodule init
